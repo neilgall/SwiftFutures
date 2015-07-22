882dc19aefc68eb7280fc8f658c27c2f6546cfee
Neil Gall <neilgall@mac.com>
2015-07-22 22:18:20 +0100

    Initial Commit

---
+++ SwiftFutures/SwiftFutures.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFutures.h
+//  SwiftFutures
+//
+//  Created by Neil Gall on 22/07/2015.
+//  Copyright © 2015 Neil Gall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFutures.
+FOUNDATION_EXPORT double SwiftFuturesVersionNumber;
+
+//! Project version string for SwiftFutures.
+FOUNDATION_EXPORT const unsigned char SwiftFuturesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFutures/PublicHeader.h>
+
+
